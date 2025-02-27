cb419b466684328516aef324c3c1c67467d69e1e
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-02-27 17:24:01 -0300

    first commit

---
+++ Ejercicio1.js
@@ -0,0 +1,50 @@
+//Ejercicio 1
+let numero1 = 1
+
+let stringUno = '1'
+
+if (numero1 == stringUno) {
+
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+  
+  } else {
+  
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+  
+  }
+//Ejercicio 2
+let numero30 = 30
+
+let stringTreinta = '30'
+
+if (numero30 === stringTreinta) {
+
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
+  
+  } else {
+  
+    console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
+  
+  }
+//Ejercicio 3
+let numero10 = 10
+
+let stringDiez = '10'
+
+if (numero10 == stringDiez) {
+
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
+  
+  } else {
+  
+    console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
+  
+  }
+
+    //let nombre;
+    //let edad;
+    //let materiaProgramacion;
+
+    
+
+ 
