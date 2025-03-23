59451df4daa9bf337bcdf8ffc1d1a208e396b292
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-03-23 11:44:53 -0300

    practicas

--- bucles.js
+++ bucles.js
@@ -5,3 +5,25 @@ for(let i = 1; i <5; i++ ){
     console.log(`hola ${i}`)
 }
 //Estructura para almacenar varios valores
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(let i=3; i<numbers.length; i++){
+    console.log(`Elemonto es: ${numbers[i]}`)
+}
+
+//While
+let i = 0
+while(i<5){
+    console.log(`Hola ${i}`)
+    i++
+}
+
+//breack y Continue
+for (let i=0; i<10; i++){
+    if(i == 5){
+        continue
+    }else if(i == 6){
+        break
+    }
+    console.log(` Hola Kalcha: ${i}`)
+}
