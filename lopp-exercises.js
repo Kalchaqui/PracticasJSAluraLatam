8189edad194221b34ea6be82c981ce443c2ace58
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-03-30 23:51:21 -0300

    practicas

--- lopp-exercises.js
+++ lopp-exercises.js
@@ -6,16 +6,43 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
 // NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
 
 // 1. Crea un bucle que imprima los números del 1 al 20
+//for(i=1; i<=20; i++){
+  //  console.log(`numeros: ${i}`)
+//}
+
 
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
 
+//for(i=0; i<100; i++){
+  //  console.log(`numeros: ${i+1}`)
+//}
+
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
 
+//for(i=1; i<50; i++){
+//console.log(`numeros: ${i*2}`)
+//}
+//breack y Continue
+//for (let i = 2; i <= 50; i += 2) {
+  //  console.log(`Hola Kalcha: ${i}`);
+//}
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+
+//const nombres = ["ana", "Carlos", "Beatriz", "David", "Elena"]
+
+//for(let i=0; i<nombres.length; i++){
+  //  console.log(nombres[i])
+//}
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+const numeros = [2, 20, 50, 17, 38]
+let producto = 1// Inicializamos en 1 porque multiplicar por 0 daría siempre 0
+for (let i = 0; i< numeros.length; i++) {
+    producto *= numeros[i]; // Multiplicamos cada número
+      console.log(`La multiplicación es: ${producto}`);
+  }
 
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
 
